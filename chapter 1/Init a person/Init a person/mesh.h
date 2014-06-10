0e5b1ca9a2cd7b1e7ceb1fcedad2d1fa1e648ba5
hahaMiffi <123miffi123@gmail.com>
2014-06-10 13:21:55 +0800

    example 1

---
+++ chapter 1/Init a person/Init a person/mesh.h
@@ -0,0 +1,26 @@
+#ifndef _MESH_
+#define _MESH_
+
+#include <d3dx9.h>
+#include <vector>
+
+using namespace std;
+
+class Mesh
+{
+public:
+	Mesh();
+	Mesh( char fName[] );
+	~Mesh();
+	HRESULT Load( char fName[] );
+	void Render();
+	void Release();
+
+private:
+	ID3DXMesh* m_pMesh;
+	vector<IDirect3DTexture9*> m_textures;
+	vector<D3DMATERIAL9> m_materials;
+	D3DMATERIAL9 m_white;
+};
+
+#endif
